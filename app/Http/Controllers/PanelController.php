1254bea3009b9a41f9d4f40865abd9f9b56d9362
Berkay-Cubuk <berkaycubuk@gmail.com>
2020-07-02 11:02:50 +0300

    - login system works

--- app/Http/Controllers/PanelController.php
+++ app/Http/Controllers/PanelController.php
@@ -8,11 +8,19 @@ use App\User;
 class PanelController extends Controller
 {
     public function index() {
-
+        if(session('token') != null) {
+            return view('panel/home');
+        } else {
+            return redirect()->route('home');
+        }
     }
 
     public function login() {
-        return view('login');
+        if(session('token') != null) {
+            return redirect()->route('panel.index');
+        } else {
+            return view('login');
+        }
     }
 
     public function loginRequest() {
@@ -22,8 +30,10 @@ class PanelController extends Controller
         ])->first();
 
         if($user) {
+            session(['token' => $user->id]);
             return redirect('/login')->with('message', 'Welcome!');
         } else {
+            session(['token' => null]);
             return redirect('/login')->with('message', 'Wrong username or password!');
         }
     }
