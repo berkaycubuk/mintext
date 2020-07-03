6892742f5654997c5e0fabbc1570ce12492f7a56
Berkay-Cubuk <berkaycubuk@gmail.com>
2020-07-03 18:35:16 +0300

    - panel css created

--- resources/views/panel/main.blade.php
+++ resources/views/panel/main.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Panel - @yield('title')</title>
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/panel.css') }}">
 </head>
 <body>
     <header class="header">
